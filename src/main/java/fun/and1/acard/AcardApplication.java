7c3da426df84a594682c607879bb180a43bbaa40
kevinzcf <kevinzcf@gmail.com>
2020-04-03 22:52:03 +0200

    init springboot

---
+++ src/main/java/fun/and1/acard/AcardApplication.java
@@ -0,0 +1,13 @@
+package fun.and1.acard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fun/and1/acard/AcardApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.and1.acard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
